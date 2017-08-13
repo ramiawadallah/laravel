33f08a4370dec7912827799e5682ea73ada52813
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-08-13 10:17:34 +0300

    update bootstrap front-end design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,67 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>{{ trans('main.logo')}}</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Styles -->
-        <style>
-            html, body {
-                background-color:  #970048;
-                color: #fff;
-                font-family: 'Raleway', 'Cairo', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #fff;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+    <link href="public/css/bootstrap/bootstrap.css" rel="stylesheet">
+    <link href="public/css/frontend/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -75,46 +19,58 @@
     <![endif]-->
   </head>
   <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">{{ trans('main.logo')}}</a>
+        </div>
 
-    <div class="container">
-        <div class="right">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+          <ul class="nav navbar-nav navbar-right">
             @if (Route::has('login'))
-                <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                    <li><a href="{{ url('/home') }}">Home</a></li>
                     @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
                     @endif
-                </div>
             @endif
-         </div>
-    </div>
-    
+            
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    Language <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                    @foreach(LaravelLocalization::getSupportedLocales() as $key => $value)
+                        <li>
+                            <a rel="alternate" hreflang="{{ $key }}" href="{{ LaravelLocalization::getLocalizedURL($key) }}">
+                                {{ $value['native'] }}
+                            </a>
+                        </li>                                    
+                     @endforeach
+                </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
 
     <div class="container">
-        <div class="col-md-5">
-            <div>
-              <!-- Nav tabs -->
-              <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-                <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
-              </ul>
 
-              <!-- Tab panes -->
-              <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="home">home</div>
-                <div role="tabpanel" class="tab-pane" id="profile">profile</div>
-                <div role="tabpanel" class="tab-pane" id="messages">...</div>
-                <div role="tabpanel" class="tab-pane" id="settings">...</div>
-              </div>
-
-            </div>
-
+        <!-- Start Form Col -->
+        <div class="col-xs-5">
             <form action="{{ url('/newpost') }}" method="post">
-                {{ csrf_field() }}
+                    {{ csrf_field() }}
 
                     {!! BuildFields('title', null, 'text', ['id'=>'name', 'data-id' => '10']) !!}
                     <br>
@@ -122,23 +78,24 @@
                     <br>
                     <input style="background-color: #26CAD3 !important; border: 1px solid #26CAD3;" type="submit" name="btn" class="btn-block btn-lg btn-danger">
 
-            </form>
+            </form>  
         </div>
+        <!-- End Form Col -->
 
-        <div class="col-md-7">
+        <!-- Start Post Col -->
+        <div class="col-xs-7">
             @foreach($posts as $post)
                 <h2 style="color: #FFD923;">{{ unserialize($post['title'])[LaravelLocalization::getCurrentLocale()] }}</h2>
                 <p>{{ unserialize($post['body'])[LaravelLocalization::getCurrentLocale()] }}</p>
                 <hr>
             @endforeach()
         </div>
+        <!-- End Post Col -->
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="public/js/bootstrap.js"></script>
   </body>
 </html>
